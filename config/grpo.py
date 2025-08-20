2fc19f303519366074656766a3e846cf752f222f
Jayce-Ping <315229706@qq.com>
2025-08-20 13:06:43 +0800

    update

--- config/grpo.py
+++ config/grpo.py
@@ -364,10 +364,10 @@ def consistency_sd3_4gpu():
 
     config.resolution = 1024
     config.max_sequence_length = 512
-    config.sample.train_batch_size = 4
+    config.sample.train_batch_size = 2
     config.sample.num_image_per_prompt = 16
     config.sample.num_batches_per_epoch = int(16/(gpu_number*config.sample.train_batch_size/config.sample.num_image_per_prompt))
-    # 16 / (4 * 4 / 16)
+    # 16 / (4 * 2 / 16)
     assert config.sample.num_batches_per_epoch % 2 == 0, "Please set config.sample.num_batches_per_epoch to an even number! This ensures that config.train.gradient_accumulation_steps = config.sample.num_batches_per_epoch / 2, so that gradients are updated twice per epoch."
     config.sample.test_batch_size = 8
 
