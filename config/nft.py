9bfa356c8b9dfa391f775073a092128e10852536
Jayce-Ping <315229706@qq.com>
2025-10-20 21:24:18 +0800

    Update dataset

--- config/nft.py
+++ config/nft.py
@@ -15,9 +15,8 @@ spec.loader.exec_module(base)
 FLUX_MODEL_PATH = "black-forest-labs/FLUX.1-dev"
 # FLUX_MODEL_PATH = "/root/siton-data-51d3ce9aba3246f88f64ea65f79d5133/.cache/huggingface/hub/models--black-forest-labs--FLUX.1-dev/snapshots/3de623fc3c33e44ffbe2bad470d0f45bccf2eb21"
 # SAVE_DIR = 'logs'
-SAVE_DIR = '/scratch/users/astar/ares/cp3jia/Flow_NFT/logs'
-# SAVE_DIR = '/root/siton-tmp/Flow_NFT/logs'
-# SAVE_DIR = '/home/users/astar/ares/cp3jia/scratch/Flow_NFT/logs'
+# SAVE_DIR = '/scratch/users/astar/ares/cp3jia/Flow_NFT/logs'
+SAVE_DIR = '/root/siton-tmp/Flow_NFT/logs'
 
 # --------------------------------------------------base------------------------------------------------------------
 def compressibility():
@@ -100,7 +99,7 @@ def subclipI_flux():
     gpu_number = 4
     config = compressibility()
 
-    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/train_half_2by2")
+    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/half_2by2")
     config.prompt_fn = "geneval"
     config.pretrained.model = FLUX_MODEL_PATH
     config.enable_mem_log = False
@@ -268,18 +267,19 @@ def pickscore_flux():
     return config
     
 
-def consistencyReward_clip_flux():
-    gpu_number = 7
+def consistencyReward_clip():
+    gpu_number = 2
     config = compressibility()
 
-    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/train_half_2by2")
+    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/half_2by2_mini_all")
     config.prompt_fn = "geneval"
     config.pretrained.model = FLUX_MODEL_PATH
     config.enable_mem_log = False
-    # config.logging_platform = "swanlab"
+    config.logging_platform = "swanlab"
+    config.run_name = 'consistencyReward-clip-test-mini'
 
     config.enable_flexible_size = False
-    config.resolution = 1024
+    config.resolution = 512
     config.max_sequence_length = 512
 
     # Testing
@@ -289,30 +289,30 @@ def consistencyReward_clip_flux():
 
     # Sampling
     ## sliding window scheduler
-    config.sample.cps = True
-    config.sample.num_steps = 8
+    config.sample.cps = False
+    config.sample.num_steps = 10
     config.sample.use_sliding_window = True
     config.sample.window_size = 1
     config.sample.left_boundary = 1
     config.sample.noise_steps = [1]
-    config.sample.noise_level = 0.8
+    config.sample.noise_level = 0.7
     config.sample.merge_step = 0
-    config.sample.global_std = True
+    config.sample.global_std = False
     config.sample.use_history = False
     config.sample.same_latent = False
     config.sample.guidance_scale = 3.5
-    config.sample.subfig_permutation = True
+    config.sample.subfig_permutation = False
 
     ## batches
     config.enable_gradient_checkpointing = False
     config.sample.batch_size = 1
-    config.sample.num_images_per_prompt = 4
+    config.sample.num_images_per_prompt = 16
     config.sample.max_group_size = 16
-    config.sample.unique_sample_num_per_epoch = 42 # Number of unique prompts used in each epoch all gathered
+    config.sample.unique_sample_num_per_epoch = 16 # Number of unique prompts used in each epoch all gathered
     config.sample.sample_num_per_epoch = math.lcm(
         config.sample.max_group_size * config.sample.unique_sample_num_per_epoch,
         gpu_number * config.sample.batch_size
-    ) # Total number of sample on all processes
+    ) # Total number of samples on all processes
 
     # Update number of unique prompt per epoch and check balance
     unique_sample_num_per_epoch = config.sample.sample_num_per_epoch // config.sample.max_group_size
@@ -323,7 +323,8 @@ def consistencyReward_clip_flux():
     config.sample.num_batches_per_epoch = int(config.sample.sample_num_per_epoch / (gpu_number * config.sample.batch_size))
 
     # Training
-    config.train.loss_type = 'nft'
+    config.train.param_noise_std = 0
+    config.train.loss_type = 'ppo'
     config.train.batch_size = config.sample.batch_size
     config.train.learning_rate = 3e-4
     config.train.gradient_step_per_epoch = 1 if 'nft' in config.train.loss_type else 2
@@ -339,20 +340,21 @@ def consistencyReward_clip_flux():
 
     config.train.ema = True
     config.per_prompt_stat_tracking = True
-    config.save_freq = 10 # epoch
-    config.eval_freq = 10 # 0 for no eval applied
+    config.save_freq = 5 # epoch
+    config.eval_freq = 5 # 0 for no eval applied
 
     config.reward_fn = {
-        "consistency_score": 0.2,
-        "subfig_clipT" : 0.8
+        "consistency_score": 2,
+        "subfig_clipT" : 8
     }
-    agg_fn = None
+
+    agg_fn = geometric_mean
 
     config.aggregate_fn_code = inspect.getsource(agg_fn) if agg_fn is not None else None
 
     config.aggregate_fn = agg_fn
 
-    config.save_dir = os.path.join(SAVE_DIR, f'ConsistencyReward-subclip', f'flux-{gpu_number}gpu-2by2-half_grid-times-consistency-plus-clipT_8steps')
+    config.save_dir = os.path.join(SAVE_DIR, f'test', f'test')
 
     return config
 
@@ -360,7 +362,7 @@ def grid_consistency_clip_nft_perm():
     gpu_number = 4
     config = compressibility()
 
-    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/train_half_2by2")
+    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/half_2by2")
     config.prompt_fn = "geneval"
     config.pretrained.model = FLUX_MODEL_PATH
     config.enable_mem_log = False
@@ -450,7 +452,7 @@ def grid_consistency_clip_nft():
     gpu_number = 4
     config = compressibility()
 
-    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/train_half_2by2")
+    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/half_2by2")
     config.prompt_fn = "geneval"
     config.pretrained.model = FLUX_MODEL_PATH
     config.enable_mem_log = False
@@ -540,7 +542,7 @@ def grid_consistency_clip_nft_step():
     gpu_number = 4
     config = compressibility()
 
-    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/train_half_2by2")
+    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/half_2by2")
     config.prompt_fn = "geneval"
     config.pretrained.model = FLUX_MODEL_PATH
     config.enable_mem_log = False
@@ -631,7 +633,7 @@ def grid_consistency_clip_ppo_sde():
     gpu_number = 4
     config = compressibility()
 
-    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/train_half_2by2")
+    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/half_2by2")
     config.prompt_fn = "geneval"
     config.pretrained.model = FLUX_MODEL_PATH
     config.enable_mem_log = False
@@ -721,7 +723,7 @@ def grid_consistency_clip_ppo_sde_perm():
     gpu_number = 2
     config = compressibility()
 
-    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/train_half_2by2")
+    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/half_2by2")
     config.prompt_fn = "geneval"
     config.pretrained.model = FLUX_MODEL_PATH
     config.enable_mem_log = False
@@ -813,7 +815,7 @@ def grid_consistency_clip_flux():
     gpu_number = 4
     config = compressibility()
 
-    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/train_half_2by2")
+    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/half_2by2")
     config.prompt_fn = "geneval"
     config.pretrained.model = FLUX_MODEL_PATH
     config.enable_mem_log = False
@@ -903,7 +905,7 @@ def grid_consistency_clip_flux_cps():
     gpu_number = 4
     config = compressibility()
 
-    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/train_half_2by2")
+    config.dataset = os.path.join(os.getcwd(), "dataset/T2IS/half_2by2")
     config.prompt_fn = "geneval"
     config.pretrained.model = FLUX_MODEL_PATH
     config.enable_mem_log = False
