58d90b0521adce7dd626628a7ac69fbfe4291eb2
Jayce-Ping <315229706@qq.com>
2025-11-03 22:37:41 +0800

    Update config

--- config/flux.py
+++ config/flux.py
@@ -7,6 +7,9 @@ import inspect
 import numpy as np
 from scipy.stats import gmean, hmean
 import torch
+from datetime import datetime
+
+time_stamp = datetime.now().strftime("%Y%m%d-%H%M%S")
 
 spec = spec_from_file_location('base', os.path.join(os.path.dirname(__file__), "base.py"))
 base = module_from_spec(spec)
@@ -135,15 +138,27 @@ def get_log_tamed_aggregate_fn(delta: float = 0.2, epsilon: float = 1e-4):
 
     return agg_fn
 
+def standardize_aggregate_fn(**kwargs):
+    values = np.stack(
+        [v for v in kwargs.values() if v is not None],
+        axis=0
+    ) # (num_rewards, sample_num) or (num_rewards, num_time_steps, sample_num)
+    means = np.mean(values, axis=-1, keepdims=True)
+    stds = np.std(values, axis=-1, keepdims=True) + 1e-4
+    standardized = (values - means) / stds
+    aggregated = np.sum(standardized, axis=0)
+    return aggregated
+
 
 # -----------------------------------------------------------Flux---------------------------------------------------------------
 
-def generate_ConsistencyReward_clip_config_for_resolution_exp(
-    run_name: str,
-    save_dir_suffix: str,
-    resolution: int,
+def standardize_aggregate_test():
+    run_name: str = 'standardize_aggregate_test'
+    save_dir_suffix: str = 'standardize_aggregate_test'
+    resolution: int = 512
     auto_log_tame: bool = False
-):
+    prompt_template_version: int = 0
+    weights: tuple[float, float] = (1.0, 1.0)
     assert resolution in [256, 384, 512, 720, 1024], f"Unsupported resolution: {resolution}"
     gpu_number = get_gpu_count()
     config = compressibility()
@@ -170,20 +185,16 @@ def generate_ConsistencyReward_clip_config_for_resolution_exp(
     config.save_freq = 10 # epoch
     config.eval_freq = 10 # 0 for no eval applied
     config.train.reward_fn = {
-        "consistency_score": 0.2,
-        "subfig_clipT" : 1
+        "consistency_score": weights[0],
+        "subfig_clipT" : weights[1]
     }
     config.train.reward_fn_kwargs = {
         'model': 'ConsistencyReward-7B-CoT-09',
         'port': 8000,
-        "prompt_template_version": 1,
+        "prompt_template_version": prompt_template_version
     }
-    if auto_log_tame:
-        agg_fn = get_log_tamed_aggregate_fn(delta=0.2, epsilon=1e-4)
-    else:
-        def agg_fn(consistency_score : np.ndarray, subfig_clipT : np.ndarray) -> np.ndarray:
-            return np.log(1 + consistency_score) + subfig_clipT
-
+    agg_fn = standardize_aggregate_fn
+    
     config.train.aggregate_fn_code = inspect.getsource(agg_fn) if agg_fn is not None else None
     config.train.aggregate_fn = agg_fn
 
@@ -192,9 +203,9 @@ def generate_ConsistencyReward_clip_config_for_resolution_exp(
         # 'model': 'InternVL'
         'model': 'ConsistencyReward-7B-CoT-09',
         'port': 8000,
-        "prompt_template_version": 1,
+        "prompt_template_version": prompt_template_version,
     }
-    config.train.aggregate_fn_code = config.train.aggregate_fn_code
+    config.test.aggregate_fn_code = config.train.aggregate_fn_code
     config.test.aggregate_fn = config.train.aggregate_fn
 
     # Testing
@@ -265,119 +276,195 @@ def generate_ConsistencyReward_clip_config_for_resolution_exp(
 
     return config
 
+def generate_ConsistencyReward_clip_config_for_resolution_exp(
+    run_name: str,
+    save_dir_suffix: str,
+    resolution: int,
+    auto_log_tame: bool = False,
+    prompt_template_version: int = 0,
+    weights: tuple[float, float] = (0.2, 1.0)
+):
+    assert resolution in [256, 384, 512, 720, 1024], f"Unsupported resolution: {resolution}"
+    gpu_number = get_gpu_count()
+    config = compressibility()
+    dataset_map = {
+        256: "dataset/T2IS/half_2by2_micro_train",
+        384: "dataset/T2IS/half_2by2_mini_train",
+        512: "dataset/T2IS/half_2by2_small_train",
+        720: "dataset/T2IS/half_2by2_medium_train",
+        1024: "dataset/T2IS/half_2by2"
+    }
+    config.dataset = os.path.join(os.getcwd(), dataset_map[resolution])
+    config.resolution = resolution
+    config.train.resolution = resolution
+    config.test.resolution = 1024 # Keep test resolution to 1024 for evaluation
 
-def consistencyReward_clip_ori():
-    run_name = 'H200, PPO, 1s+log(1+0.2cot), 10sde, noise=0.7 at [1], original_size, groupstd,'
-    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-original'
-    resolution = 1024
-    config = generate_ConsistencyReward_clip_config_for_resolution_exp(
-        run_name=run_name,
-        save_dir_suffix=save_dir_suffix,
-        resolution=resolution,
-    )
+    config.enable_flexible_size = False
+    config.prompt_fn = "geneval"
+    config.pretrained.model = FLUX_MODEL_PATH
+    config.enable_mem_log = False
+    # config.logging_platform = "swanlab"
+
+    config.run_name = run_name
+    # Add time stamp to save dir suffix to avoid overwriting
+    save_dir_suffix = f"{save_dir_suffix}-{time_stamp}"
+    config.save_dir = os.path.join(SAVE_DIR, f'consistencyReward-subclip', save_dir_suffix)
+    config.save_freq = 10 # epoch
+    config.eval_freq = 10 # 0 for no eval applied
+    config.train.reward_fn = {
+        "consistency_score": weights[0],
+        "subfig_clipT" : weights[1]
+    }
+    config.train.reward_fn_kwargs = {
+        'model': 'ConsistencyReward-7B-CoT-01',
+        'port': 8000,
+        "prompt_template_version": prompt_template_version
+    }
+    if auto_log_tame:
+        agg_fn = get_log_tamed_aggregate_fn(delta=0.15, epsilon=1e-4)
+    else:
+        def agg_fn(consistency_score : np.ndarray, subfig_clipT : np.ndarray) -> np.ndarray:
+            return np.log(1 + consistency_score) + subfig_clipT
+
+    config.train.aggregate_fn_code = inspect.getsource(agg_fn) if agg_fn is not None else None
+    config.train.aggregate_fn = agg_fn
+
+    config.test.reward_fn = config.train.reward_fn
+    config.test.reward_fn_kwargs = config.train.reward_fn_kwargs
+    config.test.aggregate_fn_code = config.train.aggregate_fn_code
+    config.test.aggregate_fn = config.train.aggregate_fn
+
+    # Testing
+    config.test.save_eval_images = True
+    config.test.batch_size = 5
+    config.test.num_steps = 20
+    config.test.merge_step = 0
+
+    # Sampling
+    ## sliding window scheduler
+    config.sample.global_std = True
+    config.sample.use_history = False
+    config.sample.same_latent = False
+    config.sample.guidance_scale = 3.5
+    config.sample.subfig_permutation = False
+
+    config.sample.cps = False
+    config.sample.num_steps = 10
+    config.sample.noise_steps = [1]
+    config.sample.noise_level = 0.7
+    config.sample.merge_step = 0
+
+    ## batches
+    config.enable_gradient_checkpointing = False
+    config.sample.batch_size = 1
+    config.sample.reward_batch_size = min(config.sample.batch_size * 4, 8)
+    config.sample.num_images_per_prompt = 16
+    config.sample.max_group_size = config.sample.num_images_per_prompt or 16
+    unique_sample_num_range = range(42, 50)
+    # config.sample.unique_sample_num_per_epoch = 48 # Number of unique prompts used in each epoch all gathered
+    config.sample.unique_sample_num_per_epoch = None # Number of unique prompts used in each epoch all gathered
+
+    for num in unique_sample_num_range:
+        total_samples = num * config.sample.num_images_per_prompt
+        if total_samples % (gpu_number * config.sample.batch_size) == 0:
+            config.sample.unique_sample_num_per_epoch = num
+            break
+    assert config.sample.unique_sample_num_per_epoch is not None, f"Cannot find proper unique_sample_num_per_epoch in range {list(unique_sample_num_range)}, please check your configuration!"
+    
+    config.sample.sample_num_per_epoch = math.lcm(
+        config.sample.num_images_per_prompt * config.sample.unique_sample_num_per_epoch,
+        gpu_number * config.sample.batch_size
+    ) # Total number of samples on all processes
+
+    # Update number of unique prompt per epoch
+    config.sample.unique_sample_num_per_epoch = config.sample.sample_num_per_epoch // config.sample.max_group_size
+
+    # number of batches per epoch per GPU
+    config.sample.num_batches_per_epoch = int(config.sample.sample_num_per_epoch / (gpu_number * config.sample.batch_size))
+
+    # Training
+    config.train.param_noise_std = 0
+    config.train.loss_type = 'ppo'
+    config.train.batch_size = config.sample.batch_size
+    config.train.learning_rate = 3e-4
+    config.train.gradient_step_per_epoch = 1 if 'nft' in config.train.loss_type else 2
+    assert config.sample.num_batches_per_epoch % config.train.gradient_step_per_epoch == 0, f"""Make sure num_batches_per_epoch is divisible by gradient_step_per_epoch."""
+    config.train.gradient_accumulation_steps = config.sample.num_batches_per_epoch // config.train.gradient_step_per_epoch
+    config.train.num_inner_epochs = 1
+    config.train.timestep_fraction = 1
+    config.train.guidance_scale = 3.5
+    config.train.timesteps = config.sample.noise_steps # Train on all noise steps
+    config.train.beta = 0
+    config.train.nft_beta = 1
+    config.train.decay_type = 1 if 'nft' in config.train.loss_type else 0
+    config.train.ema = True
+    config.per_prompt_stat_tracking = True
+
     return config
 
+
+
 def consistencyReward_clip_ori_auto_tame():
-    run_name = 'H200, PPO, auto-tame, 1s+log(1+0.2cot), 10sde, noise=0.7 at [1], original_size, groupstd,'
-    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-original-auto_tame'
+    prompt_template_version = 0
+    weights = (0.2, 1.0)
+    run_name = f'H200, PPO, auto-tame, {weights[1]}s+{weights[0]}cot, 10sde, noise=0.7 at [1], original_size, groupstd, v{prompt_template_version}'
+    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-original-auto_tame-v{prompt_template_version}'
     resolution = 1024
     config = generate_ConsistencyReward_clip_config_for_resolution_exp(
         run_name=run_name,
         save_dir_suffix=save_dir_suffix,
         resolution=resolution,
         auto_log_tame=True,
-    )
-    return config
-
-def consistencyReward_clip_medium():
-    run_name = 'H200, PPO, 1s+log(1+0.2cot), 10sde, noise=0.7 at [1], medium, groupstd,'
-    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-medium'
-    resolution = 720
-    config = generate_ConsistencyReward_clip_config_for_resolution_exp(
-        run_name=run_name,
-        save_dir_suffix=save_dir_suffix,
-        resolution=resolution,
+        prompt_template_version=prompt_template_version,
+        weights=weights,
     )
     return config
 
 def consistencyReward_clip_medium_auto_tame():
-    run_name = 'H200, PPO, auto-tame, 1s+log(1+0.2cot), 10sde, noise=0.7 at [1], medium, groupstd,'
-    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-medium-auto_tame'
+    prompt_template_version = 0
+    weights = (0.1, 1.0)
+    run_name = f'H200, PPO, auto-tame, {weights[1]}s+{weights[0]}cot, 10sde, noise=0.7 at [1], medium, groupstd, v{prompt_template_version}'
+    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-medium-auto_tame-v{prompt_template_version}'
     resolution = 720
     config = generate_ConsistencyReward_clip_config_for_resolution_exp(
         run_name=run_name,
         save_dir_suffix=save_dir_suffix,
         resolution=resolution,
         auto_log_tame=True,
+        prompt_template_version=prompt_template_version,
+        weights=weights,
     )
     return config
 
-def consistencyReward_clip_small():
-    run_name = 'H200, PPO, 1s+log(1+0.2cot), 10sde, noise=0.7 at [1], small, groupstd,'
-    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-small'
-    resolution = 512
-    config = generate_ConsistencyReward_clip_config_for_resolution_exp(
-        run_name=run_name,
-        save_dir_suffix=save_dir_suffix,
-        resolution=resolution,
-    )
-    return config
-
-def consistencyReward_clip_small_auto_tame():
-    run_name = 'H200, PPO, auto-tame, 1s+log(1+0.2cot), 10sde, noise=0.7 at [1], small, groupstd,'
-    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-small-auto_tame'
+def consistencyReward_clip_small_v0():
+    prompt_template_version = 0
+    weights = (0.2, 1.0)
+    run_name = f'H200, PPO, auto-tame, {weights[1]}s+{weights[0]}cot, 10sde, noise=0.7 at [1], small, global_std, v{prompt_template_version}'
+    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-small-auto_tame-v{prompt_template_version}'
     resolution = 512
     config = generate_ConsistencyReward_clip_config_for_resolution_exp(
         run_name=run_name,
         save_dir_suffix=save_dir_suffix,
         resolution=resolution,
         auto_log_tame=True,
+        prompt_template_version=prompt_template_version,
+        weights=weights,
     )
     return config
 
-def consistencyReward_clip_mini():
-    run_name = 'H200, PPO, 1s+log(1+0.2cot), 10sde, noise=0.7 at [1], mini, groupstd,'
-    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-mini'
-    resolution = 384
-    config = generate_ConsistencyReward_clip_config_for_resolution_exp(
-        run_name=run_name,
-        save_dir_suffix=save_dir_suffix,
-        resolution=resolution,
-    )
-    return config
-
-def consistencyReward_clip_mini_auto_tame():
-    run_name = 'H200, PPO, auto_tame 1s+log(1+0.2cot), 10sde, noise=0.7 at [1], mini, groupstd,'
-    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-mini-auto_tame'
-    resolution = 384
+def consistencyReward_clip_small_v1():
+    prompt_template_version = 1
+    weights = (0.2, 1.0)
+    run_name = f'H200, PPO, auto-tame, {weights[1]}s+{weights[0]}cot, 10sde, noise=0.7 at [1], small, global_std, v{prompt_template_version}'
+    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-small-auto_tame-v{prompt_template_version}'
+    resolution = 512
     config = generate_ConsistencyReward_clip_config_for_resolution_exp(
         run_name=run_name,
         save_dir_suffix=save_dir_suffix,
         resolution=resolution,
         auto_log_tame=True,
-    )
-    return config
-
-def consistencyReward_clip_micro():
-    run_name = 'H200, PPO, 1s+log(1+0.2cot), 10sde, noise=0.7 at [1], micro, groupstd,'
-    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-micro'
-    resolution = 256
-    config = generate_ConsistencyReward_clip_config_for_resolution_exp(
-        run_name=run_name,
-        save_dir_suffix=save_dir_suffix,
-        resolution=resolution,
-    )
-    return config
-
-def consistencyReward_clip_micro_auto_tame():
-    run_name = 'H200, PPO, auto_tame, 1s+log(1+0.2cot), 10sde, noise=0.7 at [1], micro, groupstd,'
-    save_dir_suffix = f'10s-log-2cot_ppo_10sde_train1_groupstd_train-micro-auto_tame'
-    resolution = 256
-    config = generate_ConsistencyReward_clip_config_for_resolution_exp(
-        run_name=run_name,
-        save_dir_suffix=save_dir_suffix,
-        resolution=resolution,
-        auto_log_tame=True,
+        prompt_template_version=prompt_template_version,
+        weights=weights,
     )
     return config
 
