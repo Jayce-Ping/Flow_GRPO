587f02f22c65e45dd665cd91a03b0e1d8aee7838
Jayce-Ping <315229706@qq.com>
2025-11-08 19:27:40 +0800

    Update config

--- config/flux.py
+++ config/flux.py
@@ -335,7 +335,7 @@ def consistencyReward_clip_small():
 
 def consistencyReward_clip_small_reproduce():
     prompt_template_version = 0
-    weights = (0.1, 1.0)
+    weights = (0.2, 0.8)
     delta = 0.2
     run_name = f'Auto-tame ({delta}), {weights[1]}s+{weights[0]}cot, small, group_std'
     save_dir_suffix = f'{weights[1]}s-{weights[0]}cot_ppo_10sde_train1_groupstd_train-small'
