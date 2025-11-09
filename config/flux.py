ccb272c9c3543cc4ee18b072ecaccb39433768fc
Jayce-Ping <315229706@qq.com>
2025-11-09 21:42:49 +0800

    Update

--- config/flux.py
+++ config/flux.py
@@ -369,7 +369,7 @@ def consistencyReward_clip_small_guard_3():
 
 def consistencyReward_clip_small():
     prompt_template_version = 0
-    weights = (0.3, 0.7)
+    weights = (0.2, 0.8)
     delta = 0.2
     run_name = f'Auto-tame ({delta}), {weights[1]}s+{weights[0]}cot, small, group_std'
     save_dir_suffix = f'{weights[1]}s-{weights[0]}cot_ppo_10sde_train1_groupstd_train-small'
@@ -384,9 +384,9 @@ def consistencyReward_clip_small():
     )
     return config
 
-def small_reproduce():
+def consistencyReward_clip_small_37():
     prompt_template_version = 0
-    weights = (0.2, 0.8)
+    weights = (0.3, 0.7)
     delta = 0.2
     run_name = f'Auto-tame ({delta}), {weights[1]}s+{weights[0]}cot, small, group_std'
     save_dir_suffix = f'{weights[1]}s-{weights[0]}cot_ppo_10sde_train1_groupstd_train-small'
@@ -399,7 +399,6 @@ def small_reproduce():
         weights=weights,
         delta=delta
     )
-    config.train.clip_range = 1e-5
     return config
 
 def consistencyReward_clip_mini():
