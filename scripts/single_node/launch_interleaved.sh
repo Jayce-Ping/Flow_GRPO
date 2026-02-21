f32fcecb3b215ca41c8d9085263f8b7ba3889409
Jayce <93087577+Jayce-Ping@users.noreply.github.com>
2026-02-21 11:01:29 +0800

    update script

--- scripts/single_node/launch_interleaved.sh
+++ scripts/single_node/launch_interleaved.sh
@@ -7,7 +7,8 @@ NUM_PROCESSES=$((NUM_MACHINES * GPUS_PER_NODE))
 MASTER_PORT=19001
 
 accelerate launch --config_file scripts/accelerate_configs/fsdp.yaml \
-    --num_machines ${NUM_MACHINES} --num_processes ${NUM_PROCESSES} \
+    --num_machines ${NUM_MACHINES} \
+    --num_processes ${NUM_PROCESSES} \
     --main_process_port ${MASTER_PORT} \
     scripts/train_interleaved_grpo.py \
-    --config config/interleaved_grpo.py
+    --config config/interleaved_grpo.py:interleaved_grpo_lora
