2faf57f9907b1e83a38062238b2b557bcd5ce0ba
Jayce-Ping <315229706@qq.com>
2026-03-19 12:42:50 +0800

    update

--- scripts/multi_node/bagel/main.sh
+++ scripts/multi_node/bagel/main.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-GPUS_PER_NODE=8
-NUM_MACHINES=4
-NUM_PROCESSES=$((NUM_MACHINES * GPUS_PER_NODE))
-MASTER_PORT=19001
-MASTER_ADDR=10.82.139.22
+GPUS_PER_NODE=${HOST_GPU_NUM:-8}
+NUM_MACHINES=${HOST_NUM:-1}
+NUM_PROCESSES=${NODE_NUM:-$((NUM_MACHINES * GPUS_PER_NODE))}
+MASTER_PORT=${MASTER_PORT:-19001}
+MASTER_ADDR=${CHIEF_IP:-${LOCAL_IP:-127.0.0.1}}
+RANK=${INDEX:-0}
 
-RANK=$1
-
-accelerate launch --config_file scripts/accelerate_configs/fsdp.yaml \
-    --num_machines ${NUM_MACHINES} --num_processes ${NUM_PROCESSES} \
-    --machine_rank ${RANK} --main_process_ip ${MASTER_ADDR} --main_process_port ${MASTER_PORT} \
+accelerate launch \
+    --config_file scripts/accelerate_configs/fsdp.yaml \
+    --num_machines ${NUM_MACHINES} \
+    --num_processes ${NUM_PROCESSES} \
+    --machine_rank ${RANK} \
+    --main_process_ip ${MASTER_ADDR} \
+    --main_process_port ${MASTER_PORT} \
     scripts/train_bagel.py \
     --config config/grpo.py:pickscore_bagel
